8dceaa8418f666438e77ac5290869612d38c63fa
Janiel Flores <122830899+JanielF@users.noreply.github.com>
2024-08-07 21:39:03 -0600

    Probando

--- client/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import 'react-native-gesture-handler';
-import Navigation from './src/Components/navigation';
-
-export default function App() {
-  return <Navigation />;
-}
